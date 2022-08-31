aed4311662312cb2e6a7337b627122c1fb32cda7
Actarus4 <112523171+Actarus4@users.noreply.github.com>
2022-08-30 23:39:14 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
+    def nouveaumot=""
     # TODO completer la fonction ici
-    return mot
+    for i in len(mot):
+         if ord(mot[i])>64:
+                if ord(mot[i])>91:
+                    if ord(mot[i])>123
+                    nouveaumot+=mot[i]
+                    else:
+                    nouveaumot+='ord(mot[i])-32'
+               else:
+                nouveaumot+='ord(mot[i])+32'
+         else:
+            nouveaumot+=mot[i]
+    return nouveaumot
 
 
 if __name__ == '__main__':
